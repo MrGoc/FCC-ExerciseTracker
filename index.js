75b90f7fd860356ab39e51be6dbae23df6e9192a
MrGoc <mrgoc@live.com>
2023-03-31 09:14:26 +0200

    feat: Added create user API

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const createUser = require("./users.js").createUser;
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+// Middleware for parsing body
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
+app.post("/api/users", (req, res) => {
+  res.json(createUser(req.body.username));
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
---
+++ users.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const mySecret = process.env["MONGO_URI"];
+
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
+/*
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connection Successful!");
+});
+*/
+let userShema = new mongoose.Schema({
+  username: String,
+});
+
+let User = mongoose.model("User", userShema, "users");
+
+const createUser = (userName) => {
+  let user = new User({ username: userName });
+  user.save();
+  return user;
+};
+
+exports.createUser = createUser;
